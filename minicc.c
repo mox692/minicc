79a02ffd43239c998e6c023973e0cdd510ab6909
mox692 <55653825+mox692@users.noreply.github.com>
2021-07-08 09:28:46 +0000

    wio

--- minicc.c
+++ minicc.c
@@ -28,6 +28,25 @@ Token *token;
 // user入力
 char *user_input;
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+// MEMO: なぜこれがいるのだろう
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -109,9 +128,10 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)){
       // MEMO: なぜp++ ?
       cur = new_token(TK_RESERVED, cur, p++);
+      cur->val = strtol(p, &p, 10);
       continue;
     }
 
@@ -131,6 +151,106 @@ Token *tokenize() {
   return head.next;
 }
 
+// Num Node以外のNodeを受け取り、右辺値と左辺値から新しいNodeを生成する.
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->lhs = lhs;
+  node->rhs = rhs;
+  node->kind = kind;
+  return node;
+}
+
+// 数値をNum Nodeとして返す.
+Node *new_node_num(int num) {
+  Node *node = calloc(1, sizeof(int));
+  node->kind = ND_NUM;
+  node->val = num;
+  return node;
+}
+
+Node *expr();
+
+// primary Nodeを返す関数.
+Node *primary() {
+  // primaryが処理するのはnumか(expr)の2パターンである
+  
+  // exprの時
+  if(consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  // それ以外の時(num)
+  return new_node_num(expect_number());
+}
+
+// mul Nodeを返す関数.
+// *か/のみのtokenを読み進め、それ以外のtokenが出現した場合はbreakする
+Node *mul() {
+  Node *node = primary();
+
+  for(;;) {
+    if(consume('*')) {
+      node = new_node(ND_MUL, node, primary()); // 第3引数でprimary()を呼んで、さらにtokenを読み進めてる点に注意.
+    } else if(consume('/')) {
+      node = new_node(ND_DIV, node, primary()); // 第3引数でprimary()を呼んで、さらにtokenを読み進めてる点に注意.
+    } else {
+      break;
+    }
+  }
+  return node;
+}
+
+// expr Nodeを返す関数.
+Node *expr() {
+  Node *node = mul();
+
+  for(;;) {
+    if(consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if(consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      break;
+    }
+  }
+  return node;
+}
+
+// Nodeを引数にとり、そのNodeに対応したasmを標準出力する
+void genAsm(Node *node) {
+  // 1.ND_NUMのとき(最後はここにきて処理が終了するはず)
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  // 2.ND_NUMでない時
+  // 左辺Nodeと右辺Nodeに再起的にgenAsmをかけてasmを生成
+  genAsm(node->lhs);
+  genAsm(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
@@ -138,31 +258,19 @@ int main(int argc, char **argv) {
   }
 
   // トークナイズする
-  // MEMO: 多分こいつがリストの先頭を指してるはずなんだが。。
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  // asmコードを出力.
+  genAsm(node);
 
-  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
